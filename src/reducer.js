0c26a8d892461890af4c6fa853d5d0bc2066d210
Dave Guymon <davidwguymon@gmail.com>
2017-08-23 10:04:07 -0600

    First commit. Axios request response coming through on button click.

---
+++ src/reducer.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const initialState={
+  tasksArray: []
+}
+
+const GET_TASKS = 'GET_TASKS';
+const GET_TASKS_PENDING = 'GET_TASKS_PENDING';
+const GET_TASKS_FULFILLED = 'GET_TASKS_FULFILLED';
+
+export default function reducer(state=initialState, action) {
+  switch(action.type){
+    case GET_TASKS_FULFILLED:
+    console.log("The payload is :", action.payload.data)
+      return Object.assign({}, state, {tasksArray: action.payload.data})
+
+    default:
+      return state;
+  }
+}
+
+export function getTasks(){
+  console.log("Reached getTasks in Reducer!")
+  return {
+    type: GET_TASKS,
+    payload: axios.get('https://practiceapi.devmountain.com/api/tasks/')
+  }
+}
